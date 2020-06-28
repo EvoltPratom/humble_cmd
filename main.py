46b0e612308b803571072878956ffcc84bfb76c4
Bishant Adhikari <43327116+EvoltPratom@users.noreply.github.com>
2020-06-28 13:08:47 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from winreg import*
+import os
+cur_dir = os.getcwd()
+# print(cur_dir)
+file_name = 'helper.py'
+execute = f'python {os.path.join(cur_dir, file_name)}'
+# print(execute)
+
+a = OpenKey(HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Command Processor", 0, KEY_WRITE)
+SetValueEx(a, "Autorun", 0, REG_SZ, execute)
+# DeleteValue(a, 'Autorun')
+CloseKey(a)
---
+++ uninstall.py
@@ -0,0 +1,5 @@
+from winreg import *
+a = OpenKey(HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Command Processor", 0, KEY_WRITE)
+DeleteValue(a, 'Autorun')
+CloseKey(a)
