1d9636ccbdc972755f85167d92f98f69701e8d47
Bishant Adhikari <43327116+EvoltPratom@users.noreply.github.com>
2020-06-29 06:47:08 +0545

    Add files via upload

--- helper.py
+++ helper.py
@@ -4,16 +4,19 @@ from bs4 import BeautifulSoup
 
 url1 = 'https://www.merriam-webster.com/word-of-the-day'
 url2 = 'https://www.keepinspiring.me/quote-of-the-day/'
+try:
+    source = requests.get(url1).content
+    soup = BeautifulSoup(source, 'lxml')
+    # print(soup)
+    word = soup.find('title').text.split('|')[0]
+    definition = soup.find(
+        'div', {'class': 'wod-definition-container'}).p.text.replace(':', '-')
 
-source = requests.get(url1).content
-soup = BeautifulSoup(source, 'lxml')
-# print(soup)
-word = soup.find('title').text.split('|')[0]
-definition = soup.find(
-    'div', {'class': 'wod-definition-container'}).p.text.replace(':', '-')
+    print('\n', word)
+    print(definition[1:])
+    s2 = BeautifulSoup(requests.get(url2).content, 'lxml').find(
+        'div', {'class': 'author-quotes'}).text
+    print(f'\nQuote of the Day\n{s2}')
+except:
+    pass
 
-print('\n', word)
-print(definition[1:])
-s2 = BeautifulSoup(requests.get(url2).content, 'lxml').find(
-    'div', {'class': 'author-quotes'}).text
-print(f'\nQuote of the Day\n{s2}')
